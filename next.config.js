c1f037072dd000370b1b83ff8bf8d4941aed9bcb
Nökkvi Reyr Guðfinnsson <nokkvi@nokkvi.is>
2023-02-15 23:18:15 +0000

    Fixing env issues

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     includePaths: [path.join(__dirname, 'styles')],
   },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_API_URL: 'https://data-api.nokkvi.is',
   },
 
 }
